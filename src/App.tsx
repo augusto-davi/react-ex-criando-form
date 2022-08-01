eb7a88122ffaba4d43c28ef001b67e3d118b2468
Davi <obrigatoriopcional@gmail.com>
2022-08-01 20:45:39 -0300

    exercicio resolvido

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,61 @@
+import { ChangeEvent, useState } from "react";
+
 const App = () => {
+  const [data, setData] = useState({
+    name: "",
+    lastName: "",
+    age: "",
+  });
+
+  function rN(e: ChangeEvent<HTMLInputElement>) {
+    setData({ ...data, name: e.target.value });
+  }
+
+  function rLn(e: ChangeEvent<HTMLInputElement>) {
+    setData({ ...data, lastName: e.target.value });
+  }
+
+  function rA(e: ChangeEvent<HTMLInputElement>) {
+    setData({ ...data, age: e.target.value });
+  }
+
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+
+  function renderName(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
+  function renderLastName(e: ChangeEvent<HTMLInputElement>) {
+    setLastName(e.target.value);
+  }
+
+  function renderAge(e: ChangeEvent<HTMLInputElement>) {
+    const inputAgeToNumber = parseInt(e.target.value);
+    setAge(inputAgeToNumber);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={data.name} onChange={rN} />
       </div>
-
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={data.lastName} onChange={rLn} />
       </div>
-
       <div>
         Idade:
-        <input type="text" />
+        <input type="number" value={data.age} onChange={rA} />
       </div>
-
-      <hr/>
-
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      <hr />
+      Olá {`${data.name} ${data.lastName}`}, tudo bem?
+      <br />
+      Você tem {data.age} anos.
     </div>
   );
-}
+};
 
 export default App;
